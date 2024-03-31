28283a18c48098c80ed6bb29e0622bdaa5cb1ee6
AnsukSinhaRoy <ansuksinharoy@gmail.com>
2024-03-31 21:38:45 +0530

    path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { ConfigurePageComponent } from './configure-page/configure-page.component';
+import { AssignFucntionsToRolesComponent } from './shared/components/assign-fucntions-to-roles/assign-fucntions-to-roles.component';
 
 export const routes: Routes = [
-    {path:"configure",component:ConfigurePageComponent},
+    {path:"configure",component:AssignFucntionsToRolesComponent,
+children:[{
+    path:"new", component:AssignFucntionsToRolesComponent
+}]},
     {path:"",component:ConfigurePageComponent}
 ];
