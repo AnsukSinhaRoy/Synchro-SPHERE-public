863af89c92c91a19428d225ad768513ade9d1397
AnsukSinhaRoy <ansuksinharoy@gmail.com>
2024-04-06 12:27:45 +0530

    select module - complete

--- src/app/configure-page/configure-page.component.ts
+++ src/app/configure-page/configure-page.component.ts
@@ -4,7 +4,6 @@ import { LeadManagementConfigComponent } from '../configure-page components/lead
 import { CustomerRelationshipManagementConfigComponent } from '../configure-page components/customer-relationship-management-config/customer-relationship-management-config.component';
 import { TravelManagementConfigComponent } from '../configure-page components/travel-management-config/travel-management-config.component';
 import { ERPModule } from '../landing-page/erpmodule.interface';
-import { LandingPageDataService } from '../services/landing-page-data.service'; 
 @Component({
   selector: 'app-configure-page',
   standalone: true,
@@ -13,18 +12,15 @@ import { LandingPageDataService } from '../services/landing-page-data.service';
   styleUrl: './configure-page.component.css'
 })
 
-export class ConfigurePageComponent implements OnInit {
-  constructor(/*private dataService: LandingPageDataService*/) {}
+export class ConfigurePageComponent {
   modules: ERPModule[] = [
     { name: 'Customer Relationship Management', checked: false },
     { name: 'Lead Management', checked: true },
-    { name: 'Travel management', checked: false }
+    { name: 'Travel management', checked: true }
   ];
   
   selectedIndex = 0;
   ngOnInit() {
-    // Find the index of the first module that is checked
-    //this.modules = this.dataService.getModules().filter(module => module.checked);
     const firstCheckedModule = this.modules.find(module => module.checked);
     if (firstCheckedModule) {
       this.selectedIndex = this.modules.indexOf(firstCheckedModule);
