ee54ace8ef1708386eace1f4a1de2129b6ac2d8c
AnsukSinhaRoy <ansuksinharoy@gmail.com>
2024-04-07 01:57:29 +0530

    landing page dialog 2

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -63,6 +63,7 @@ export class LandingPageComponent {
       //this.router.navigate(['/welcome']);
       this.dialog.open(DialogRegisterOrganizationComponent, {
         width:'600px',
+        height:'600px',
         data: { name: '', email: '', organizationName: '', phoneNumber: '' }
       });
     }
