4ee291b418c430a22a2ce7119b6803a8850a1cf6
AnsukSinhaRoy <ansuksinharoy@gmail.com>
2024-04-15 16:26:14 +0530

    RBA complete except, the data comes from landing page, and not login

--- src/app/configure-page/configure-page.component.ts
+++ src/app/configure-page/configure-page.component.ts
@@ -17,33 +17,42 @@ import { CrmDashboardComponent } from '../Dashboard-page-components/crm-dashboar
 })
 
 export class ConfigurePageComponent {
-  modulesInThisPage: ERPModule[] = [];
+  modules: ERPModule[] = [];
   selectedIndex = 0;
   selectedModuleName: string;
 
   constructor(private _landingdataService: LandingPageDataService, public _logindataservice: LoginPageDataService, private router: Router) {
     //this.modules = this.dataService.getModules().filter(module => module.checked === true);
-    this.modulesInThisPage = this._landingdataService.getModules();
-    
     this.selectedModuleName = this._landingdataService.getSelectedModule();
-    
-    if (this.modulesInThisPage.filter(module => module.checked === true).length !== 0 /*to know how many modules have been chosen*/)
-       {
-      
-        
-        const selectedModuleIndex = this.modulesInThisPage.findIndex(module => module.name === this.selectedModuleName);
+    if (this._logindataservice.modules.filter(module => module.available === true).length !== 0) {
+      this.modules =this._logindataservice.modules.filter(module => module.clickable === true)
+      const selectedModuleIndex = this.modules.findIndex(module => module.name === this.selectedModuleName);
+      if (selectedModuleIndex !== -1) {
+        // If a module with the selectedModuleName exists, select it
+        this.selectedIndex = selectedModuleIndex;
+      } else {
+        // Otherwise, select the first checked module
+        const checkedModuleIndex = this.modules.findIndex(module => module.clickable);
 
-        if (selectedModuleIndex !== -1) {
-          // If a module with the selectedModuleName exists, select it
-          this.selectedIndex = selectedModuleIndex;
-        } else {
-          // Otherwise, select the first checked module
-          const checkedModuleIndex = this.modulesInThisPage.findIndex(module => module.clickable);
-  
-          if (checkedModuleIndex !== -1) {
-            this.selectedIndex = checkedModuleIndex;
-          }
-      
+        if (checkedModuleIndex !== -1) {
+          this.selectedIndex = checkedModuleIndex;
+        }
+      }
+    }
+
+    else if (this._landingdataService.modules.filter(module => module.checked === true).length !== 0) {
+      this.modules = this._landingdataService.getModules();
+      const selectedModuleIndex = this.modules.findIndex(module => module.name === this.selectedModuleName);
+      if (selectedModuleIndex !== -1) {
+        // If a module with the selectedModuleName exists, select it
+        this.selectedIndex = selectedModuleIndex;
+      } else {
+        // Otherwise, select the first checked module
+        const checkedModuleIndex = this.modules.findIndex(module => module.clickable);
+
+        if (checkedModuleIndex !== -1) {
+          this.selectedIndex = checkedModuleIndex;
+        }
       }
     }
     else {
