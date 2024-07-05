21043617ee15d87477bfd53f3a879a0b57ce2491
AnsukSinhaRoy <ansuksinharoy@gmail.com>
2024-07-05 23:56:54 +0530

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,4 +20,6 @@ export const routes: Routes = [
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
   })
-  export class AppRoutingModule { }
+  export class AppRoutingModule {
+  }
+  
