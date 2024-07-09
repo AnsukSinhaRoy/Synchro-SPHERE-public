0f4abdb806edbad6a5dcc0b369102b73f3d02574
AnsukSinhaRoy <ansuksinharoy@gmail.com>
2024-07-09 21:17:59 +0530

    dashboard css changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,6 @@ import { NgModule } from '@angular/core';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { ThanksComponent } from './shared/thanks/thanks.component';
 import { SyncrosphereDashboardComponent } from './syncrosphere-dashboard/syncrosphere-dashboard.component';
-import { Function1Component } from './sync-native-Functions/function1/function1.component';
-import { Function2Component } from './sync-native-Functions/function2/function2.component';
 
 export const routes: Routes = [
     {path:'configure',component:ConfigurePageComponent},
@@ -16,13 +14,7 @@ export const routes: Routes = [
     {path:'landing', component:LandingPageComponent},
     {path:'welcome', component:WelcomePageComponent},
     {path:'thanks', component:ThanksComponent},
-    {
-        path: 'syncdash', component: SyncrosphereDashboardComponent, children: [
-          { path: 'function1', component: Function1Component },
-          { path: 'function2', component: Function2Component }
-          // Add other function routes here
-        ]
-      }
+    {path:'syncdash', component: SyncrosphereDashboardComponent}
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/shared/sidenav/sidenav.component.ts
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -1,18 +1,19 @@
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { Function1Component } from '../../sync-native-Functions/function1/function1.component';
 import { Function2Component } from '../../sync-native-Functions/function2/function2.component';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [RouterModule, NgFor],
+  imports: [RouterModule, NgFor, NgClass],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.css'
+  styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
   functions: any[] = [];
+  activeFunction: any;
 
   @Output() functionSelected = new EventEmitter<any>();
 
@@ -23,9 +24,16 @@ export class SidenavComponent implements OnInit {
       { name: 'Function 2', component: Function2Component }
       // Add other functions as needed
     ];
+
+    // Set the active function to the first function
+    if (this.functions.length > 0) {
+      this.activeFunction = this.functions[0];
+      this.functionSelected.emit(this.activeFunction.component);
+    }
   }
 
   selectFunction(func: any) {
+    this.activeFunction = func;
     this.functionSelected.emit(func.component);
   }
 }
